c276629c437d2945964e666551fd244236fa463a
sanjiv576 <shresthasanjiv576@gmail.com>
2025-01-10 23:12:33 +0545

    add routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 // import './App.css';
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Auth } from './pages/auth/index';
+import { ExpenseTracker } from './pages/expense-tracker/index'
 function App() {
   return (
     <div className="App">
+      <Router>
 
+        <Routes>
+          <Route path='/' exact element={<Auth />} />
+          <Route path='/e' element={<ExpenseTracker />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
